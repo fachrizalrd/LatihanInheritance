addb4b11c7382924e60e8077b0698e5e01268839
Fachrizal Rafif D <fachrizal.deninsyah@students.amikom.ac.id>
2019-05-07 14:28:56 +0700

    first commit

---
+++ LatihanInheritance/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    public class Person
+    {
+        public Person (string name , int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+
+        public string Name { set; get; }
+        public int Age { set; get; }
+
+        public void GetNameAndAge()
+        {
+            Console.WriteLine("The Person has name : {0} and age : {1}", Name, Age);
+        }
+    }
+}
---
+++ LatihanInheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Mika",20);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Thomas",35,"190324","Math");        
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Sara",19,"128425","sara12345@amikom.ac.id");
+            student.GetNameAndAge();
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
